4c1c4bb61a82354fa50aa5d135de51584bab1b4f
InexLeshnevski <52599796+InexLeshnevski@users.noreply.github.com>
2020-11-17 21:35:55 +0200

    Update lab__05-01.c

--- lab__05/lab__05-01.c
+++ lab__05/lab__05-01.c
@@ -10,20 +10,20 @@ int main()
   float step;
   float x, y;
   int sign;
-  
+
   puts("Enter step: ");
   scanf("%f", &step);
   puts("");
-  
+
   tabFunc(LOWER_BOUND, UPPER_BOUND, step);
-  
+
   puts("Enter x: ");
   scanf("%f", &x);
   puts("");
-  
+
   sign = getExpSign(x, &y);
-  
-  printf("Sign: %d \n" "Result = %f \n\n", sign, y);
-  
+
+  printf("Sign: %d \n" "Result = %lf \n\n", sign, y);
+
   return 0;
 }
