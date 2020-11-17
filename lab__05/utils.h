b64b376825fdaf19e7d117a8bb3d2da295722b82
InexLeshnevski <52599796+InexLeshnevski@users.noreply.github.com>
2020-11-17 21:34:36 +0200

    Update utils.h

--- lab__05/utils.h
+++ lab__05/utils.h
@@ -1,8 +1,5 @@
-#ifndef UTILS
-#define UTILS
+#pragma once
 
-float calcExp(float x);
+double calcExp(float x);
 void tabFunc(float lowerBound, float upperBound, float step);
 int getExpSign(float x, float * y);
-
-#endif
